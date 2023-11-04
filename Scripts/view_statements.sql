4419247c83c236facee3e8c012166cc11a6a0ff8
mikd418 <walkermikayla18@gmail.com>
2023-11-04 17:44:55 -0600

    restarting everything

--- Scripts/view_statements.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW current_device_proxy AS
-SELECT DID, DLocation, DIP, DPISP
-FROM Device;
-
---CREATE VIEW meeting_information AS
---SELECT Organization.OID, CID, CName, CAttendance, CStartDate, CEndDate, conference.HName, HState, HCity, HZip, HAddress
---FROM Conference
---JOIN Hotel ON Conference.HName = Hotel.HName
---JOIN Organization ON Conference.OID = Organization.OID;
