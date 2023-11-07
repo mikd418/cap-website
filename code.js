1f3ef0f27c61de63613638e4fa9187054a8d060d
mikd418 <walkermikayla18@gmail.com>
2023-11-07 08:36:18 -0700

    breaking it

---
+++ code.js
@@ -0,0 +1,5 @@
+function NavTo() {
+    var eqPosition = pageURL.indexOf("=");  //get the position number of "=" sign
+    var html_id = pageURL.substring(eqPosition+1,pageURL.length);  //get the string to the right side of the "=" sign
+    //few lines of required code
+    }
